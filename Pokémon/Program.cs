4bd6ba747240fe49fe4f74193ec83cb313519704
Niels De Backer <niels.debacker2003@gmail.com>
2022-03-10 22:44:42 +0100

    PkmnDic creator

--- Pokémon/Program.cs
+++ Pokémon/Program.cs
@@ -2,6 +2,11 @@
 using System.IO;
 using Newtonsoft.Json.Linq;
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Pokémon
 {
     class Program
@@ -21,28 +26,24 @@ namespace Pokémon
             string number = "$.number";
             string[] type = { "$.types[0]", "$.types[1]" };
 
-            //make pokemon objects
+            //make pokemon dictionary
+            Dictionary<string, Pokémon> PkmnDic = new Dictionary<string, Pokémon>(3);
             for (int i = 0; i < pkmnlist.Count; i++)
             {
-                //
+                JObject pkmnStats = pkmnlist[i].ToObject<JObject>();
+                PkmnDic.Add(Convert.ToString(pkmnStats.SelectToken(name)), new Pokémon());
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Naam = Convert.ToString(pkmnStats.SelectToken(name));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].HP_Base = Convert.ToInt16(pkmnStats.SelectToken(hp));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Attack_Base = Convert.ToInt16(pkmnStats.SelectToken(attack));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Defense_Base = Convert.ToInt16(pkmnStats.SelectToken(defense));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].SpecialAttack_Base = Convert.ToInt16(pkmnStats.SelectToken(spAttack));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].SpecialDefense_Base = Convert.ToInt16(pkmnStats.SelectToken(spDefense));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Speed_Base = Convert.ToInt16(pkmnStats.SelectToken(speed));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Id = Convert.ToInt16(pkmnStats.SelectToken(id));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Number = Convert.ToInt16(pkmnStats.SelectToken(number));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Type[0] = Convert.ToString(pkmnStats.SelectToken(type[0]));
+                PkmnDic[Convert.ToString(pkmnStats.SelectToken(name))].Type[1] = Convert.ToString(pkmnStats.SelectToken(type[1])); //"" if no second type*/
             }
-
-            //template for loop
-            JObject pkmnStats = pkmnlist[5].ToObject<JObject>();
-
-            Pokémon bulbasaur = new Pokémon();
-
-            bulbasaur.Naam = Convert.ToString(pkmnStats.SelectToken(name));
-            bulbasaur.HP_Base = Convert.ToInt16(pkmnStats.SelectToken(hp));
-            bulbasaur.Attack_Base = Convert.ToInt16(pkmnStats.SelectToken(attack));
-            bulbasaur.Defense_Base = Convert.ToInt16(pkmnStats.SelectToken(defense));
-            bulbasaur.SpecialAttack_Base = Convert.ToInt16(pkmnStats.SelectToken(spAttack));
-            bulbasaur.SpecialDefense_Base = Convert.ToInt16(pkmnStats.SelectToken(spDefense));
-            bulbasaur.Speed_Base = Convert.ToInt16(pkmnStats.SelectToken(speed));
-            bulbasaur.Id = Convert.ToInt16(pkmnStats.SelectToken(id));
-            bulbasaur.Number = Convert.ToInt16(pkmnStats.SelectToken(number));
-            bulbasaur.Type[0] = Convert.ToString(pkmnStats.SelectToken(type[0]));
-            bulbasaur.Type[1] = Convert.ToString(pkmnStats.SelectToken(type[1])); //"" if no second type
         }
     }
 }
